aded30979583f641fd8586f334b8c2d31cc6ea71
java <Mehdi.delvaux@gmail.com>
2018-03-18 19:43:45 +0100

    [Ajout ordonnance] Choix d'une dose en fonction d"un médicament

---
+++ ProjetSwift/Medicaments.swift
@@ -0,0 +1,22 @@
+//
+//  Medicaments.swift
+//  ProjetSwift
+//
+//  Created by java on 18/03/2018.
+//  Copyright © 2018 Maxime SOUSTELLE - Mehdi DELVAUX. All rights reserved.
+//
+
+import UIKit
+
+class Medicaments {
+    var medicamentNom : String
+    var doses : [String]
+    
+    init(medicamentNom : String, doses : [String]){
+        self.medicamentNom = medicamentNom
+        self.doses = doses
+    }
+    
+}
+
+
--- ProjetSwift/Ordonnances/NewOrdoViewController.swift
+++ ProjetSwift/Ordonnances/NewOrdoViewController.swift
@@ -21,19 +21,16 @@ class NewOrdoViewController: UIViewController, UITextFieldDelegate, UIPickerView
     var pickedMedic : String? = nil
     var pickedDose : String? = nil
    
+    var medicaments = [Medicaments]()
     
-    var typeMedicament = ["Doliprane","Smecta","Eferalgan","Voltarene"]
-    
-    var doseMedicament = ["50mg","70mg","100mg","120mg"]
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-       
         
-        pickedMedic = typeMedicament[0]
-        pickedDose = doseMedicament[0]
+        initMedicamentSeed()
     }
     
     override func didReceiveMemoryWarning() {
@@ -52,8 +49,12 @@ class NewOrdoViewController: UIViewController, UITextFieldDelegate, UIPickerView
         let debutT : String = dateDebutLabel.text ?? ""
         let finT : String = dateFinLabel.text ?? ""
         let frequence : String = frequenceLabel.text ?? ""
-        
+        guard (nomMedicament != "") && (dose != "") && (debutT != "") && (finT != "") && (frequence != "") else {
+            DialogBoxHelper.alert(view: self, withTitle: "Champ(s) manquant(s)", andMessage: "Veuillez renseigner tous les champs.")
+            return
+        }
         let ordonnance = Ordonnance(context: CoreDataManager.context)
+        //let medicament = Medicament(context: CoreDataManager.context)
         ordonnance.dateDebutOrdo = debutT
         ordonnance.dateFinOrdo = finT
         ordonnance.frequenceHebdo = frequence
@@ -72,40 +73,70 @@ class NewOrdoViewController: UIViewController, UITextFieldDelegate, UIPickerView
     
     // MARK: - Picker View management
     public func numberOfComponents(in pickerView: UIPickerView) -> Int{
-        return 1
+        return 2
     }
     // The data to return for the row and component (column) that's being passed in
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if pickerView == medicPickerView {
-            return typeMedicament[row]
+        if component == 0 {
+            return medicaments[row].medicamentNom
         }
         else {
-            return doseMedicament[row]
+            let selectedMedic = pickerView.selectedRow(inComponent: 0)
+            return medicaments[selectedMedic].doses[row]
         }
     }
     
     // The number of rows of data
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if pickerView == medicPickerView {
-            return self.typeMedicament.count
+        if component == 0 {
+            return medicaments.count
         }
         else {
-            return self.doseMedicament.count
+            let selectedMedic = pickerView.selectedRow(inComponent: 0)
+            return medicaments[selectedMedic].doses.count
         }
     }
     
     // Catpure the picker view selection
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        // This method is triggered whenever the user makes a change to the picker selection.
-        // The parameter named row and component represents what was selected.
-        if pickerView == medicPickerView {
-            self.pickedMedic = typeMedicament[pickerView.selectedRow(inComponent: 0)]
-        }
-        else {
-            self.pickedDose = doseMedicament[pickerView.selectedRow(inComponent: 0)]
-        }
         
+        pickerView.reloadComponent(1)
+        let selectedMedic = pickerView.selectedRow(inComponent: 0)
+        let selectedDose = pickerView.selectedRow(inComponent: 1)
+        pickedMedic = medicaments[selectedMedic].medicamentNom
+        pickedDose = medicaments[selectedMedic].doses[selectedDose]
+        
+
     }
+    
+    // MARK: - SEEDERS INIT
+    
+    func initMedicamentSeed(){
+        medicaments.append(Medicaments(medicamentNom : "MODOPAR", doses: ["62.5","125","250"]))
+        medicaments.append(Medicaments(medicamentNom : "MODOPAR LP", doses: ["125"]))
+        medicaments.append(Medicaments(medicamentNom : "MODOPAR DISPERSIBLE", doses: ["125"]))
+        medicaments.append(Medicaments(medicamentNom : "SINEMET", doses: ["100","250"]))
+        medicaments.append(Medicaments(medicamentNom : "SINEMET LP", doses: ["100", "200"]))
+        medicaments.append(Medicaments(medicamentNom : "STALEVO", doses: ["50","75","100","125","150","175","200"]))
+        medicaments.append(Medicaments(medicamentNom : "PARLODEL", doses: ["2.5","5","10"]))
+        medicaments.append(Medicaments(medicamentNom : "TRIVASTAL", doses: ["20"]))
+        medicaments.append(Medicaments(medicamentNom : "TRIVASTAL LP", doses: ["50"]))
+        medicaments.append(Medicaments(medicamentNom : "SIFROL", doses: ["0.18", "0.7"]))
+        medicaments.append(Medicaments(medicamentNom : "SIFROL LP", doses: ["0.26","0.52","1.05","2.1"]))
+        medicaments.append(Medicaments(medicamentNom : "REQUIP", doses: ["0.25","0.5","1","2","5"]))
+        medicaments.append(Medicaments(medicamentNom : "REQUIP LP", doses: ["2","4","8"]))
+        medicaments.append(Medicaments(medicamentNom : "NEUPRO (PATCH)", doses: ["2","4","6","8"]))
+        medicaments.append(Medicaments(medicamentNom : "MANTADIX", doses: ["100"]))
+        medicaments.append(Medicaments(medicamentNom : "AZILECT", doses: ["1"]))
+        medicaments.append(Medicaments(medicamentNom : "COMTAN", doses: ["200"]))
+        medicaments.append(Medicaments(medicamentNom : "ARTANE", doses: ["2","5"]))
+        medicaments.append(Medicaments(medicamentNom : "PARKINANE LP", doses: ["2","5"]))
+        medicaments.append(Medicaments(medicamentNom : "LEPTICUR", doses: ["10"]))
+        medicaments.append(Medicaments(medicamentNom : "LEPONEX", doses: ["25","100"]))
+        medicaments.append(Medicaments(medicamentNom : "EXELON", doses: ["1.5","3","4.5","6"]))
+        medicaments.append(Medicaments(medicamentNom : "EXELON (PATCH)", doses: ["4.6","9.5"]))
+    }
+    
 
 
 }
